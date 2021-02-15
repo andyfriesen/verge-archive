0e74e28edf91ab5ffd00d96aa6b5471e05997d3d
Francis Brazeau <hatchet2k4@gmail.com>
2021-02-15 16:30:34 -0500

    updated manifests

--- rex/renamy.py
+++ rex/renamy.py
@@ -3,5 +3,15 @@ import os
 path =  os.getcwd()
 filenames = os.listdir(path)
 
+manifest = open('manifest.txt', 'w')
+
 for filename in filenames:
-    os.rename(filename, filename.lower())
+    if filename != 'manifest.txt':
+        os.rename(filename, filename.lower())
+    manifest.write(filename.lower() + '\n')
+    
+    
+manifest.close()    
+
+    
+    
