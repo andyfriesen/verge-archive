de7ddb61a666e5c2c1ddf35eb08afb9065f4aca3
Francis Brazeau <hatchet2k4@gmail.com>
2021-02-22 16:19:47 -0500

    Hopefully fixed mv and mvbs

--- mvbs/renamy.py
+++ mvbs/renamy.py
@@ -1,24 +1,21 @@
 import os
 
 path =  os.getcwd()
-#filenames = os.listdir(path)
-
 
 manifest = open('manifest.txt', 'w')
 for (root,dirs,files) in os.walk('.', topdown=True):         
         for f in files:
-            if len(root)==1:
-                fname = (root[1:]+f).replace('\\', '/').lower()
-                #print (root[1:]+f)         
+            #if f != 'manifest.txt':
+            #    os.rename(os.path.join(root, f), os.path.join(root, f.lower()))            
+            if len(root)==1:                                
+                fname = (root[1:]+f).replace('\\', '/') #.lower()       
                 manifest.write(fname + '\n')
+            
             else: 
-                fname = (root[2:]+'/'+f).replace('\\', '/').lower()
-                manifest.write(fname + '\n')
-                #print (root[2:]+f)         
-        
-    
-    
-#manifest.close()    
+                fname = (root[2:]+'/'+f).replace('\\', '/') #.lower()
+                manifest.write(fname + '\n') 
+                
+manifest.close()    
 
     
     
